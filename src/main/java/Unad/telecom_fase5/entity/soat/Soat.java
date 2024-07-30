418e25bf224a1286cd64b64a14a34668fb7cebf5
otcz <carrillozuletaoscar@gmail.com>
2024-07-30 13:03:11 -0500

    Ultimo COMMIT

--- src/main/java/Unad/telecom_fase5/entity/soat/Soat.java
+++ src/main/java/Unad/telecom_fase5/entity/soat/Soat.java
@@ -77,7 +77,7 @@ public class Soat {
             parameters.put("runt", ((getVehiculo().getRunt())));
 
             System.out.println(getVehiculo().toString() + " AQUI TODO BIEN");
-            String path = "soatV2.jrxml";
+            String path = "src/main/java/Unad/telecom_fase5/entity/soat/soatV2.jrxml";
             FileInputStream inputStream = new FileInputStream(path);
             JasperReport jasperReport = JasperCompileManager.compileReport(inputStream);
 
